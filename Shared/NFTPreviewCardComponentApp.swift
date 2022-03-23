af802b04cba4ac62889b1ceea59571f270d3234f
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-03-23 11:58:11 +0900

    Initial Commit

---
+++ Shared/NFTPreviewCardComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFTPreviewCardComponentApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/03/23.
+//
+
+import SwiftUI
+
+@main
+struct NFTPreviewCardComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
