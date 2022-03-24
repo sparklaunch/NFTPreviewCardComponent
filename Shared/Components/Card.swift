ba34a2a2231f8524941b7e11cc4339536a13c21e
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-03-24 10:39:20 +0900

    Add the equilibrium title.

--- Shared/Components/Card.swift
+++ Shared/Components/Card.swift
@@ -11,12 +11,18 @@ struct Card: View {
     var body: some View {
         ZStack {
             Color("CardColor")
-            VStack {
+            VStack(alignment: .leading) {
                 Image("Equilibrium")
                     .resizable()
                     .aspectRatio(contentMode: .fill)
                     .clipShape(RoundedRectangle(cornerRadius: 15))
                     .shadow(radius: 5)
+                Spacer()
+                    .frame(height: 20)
+                Text("Equilibrium #3429")
+                    .foregroundColor(.white)
+                    .font(.custom("Outfit", size: 27))
+                    .fontWeight(.bold)
             }
             .padding(30)
         }
